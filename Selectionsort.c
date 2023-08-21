dece26af330982d67eb68c27fe60941cc7e7ba9f
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-08-22 00:56:22 +0530

    init

---
+++ Selectionsort.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void swap(int arr, int n){
+   
+}
+
+void insertion(int arr, int n){
+
+}
+
+int main(){
+    int arr[] ={10,7,6,4,1,3,5,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    return 0;
+}
