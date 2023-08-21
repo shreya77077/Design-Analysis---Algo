80087b92b1eb51d63c20e42bb7d1ffd304efccc0
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-08-22 00:57:35 +0530

    init

--- Selectionsort.c
+++ Selectionsort.c
@@ -13,3 +13,34 @@ int main(){
     int n = sizeof(arr)/sizeof(arr[0]);
     return 0;
 }
+//By arnav
+#include<stdio.h>
+
+int main() {
+    int n;
+    int i, j, key;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+ 
+    int arr[n];
+    printf("Enter your array: ");
+    for(i=0; i<=n-1; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i=0; i<n-1; i++) {
+        for(j=i+1; j<n; j++) {
+            if(arr[i]>arr[j]) {
+                key = arr[i];
+                arr[i] = arr[j];
+                arr[j] = key;
+            }
+        }
+    }
+
+    printf("Sorted Array: ");
+    for(i=0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
