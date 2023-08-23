3c98d1f8e21aa326112160588de1194178e89c70
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-08-23 12:27:32 +0530

    init

---
+++ .c
@@ -0,0 +1,7 @@
+//merge sort
+
+//2 sorted arrays of size m&n and find common element
+//2 unsorted subarray find the common element
+//unsorted sub array, implement using merge sort
+
+// 3. LS (CS, BS,RS) IN BOOK
---
+++ mergesort.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // create temp arrays
+    int L[n1], R[n2];
+
+    // copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // merge the temp arrays back into arr[l..r]
+    i = 0; // initial index of fir  kst subarray
+    j = 0; // initial index of second subarray
+    k = l; // initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // copy the remaining elements of L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // copy the remaining elements of R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// l is for left index and r is right index of the sub-array of arr to be sorted
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        // same as (l+r)/2, but avoids overflow for large l and h
+        int m = l + (r - l) / 2;
+
+        // sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+// function to print an array
+void printArray(int A[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+    return 0;
+}
