c1dff545f0233da2da02456f56484a987426ee33
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-09-11 23:20:46 +0530

    init

--- .c
+++ .c
@@ -5,3 +5,14 @@
 //unsorted sub array, implement using merge sort
 
 // 3. LS (CS, BS,RS) IN BOOK
+
+//heap
+//cbt=(2^k)-1(max)
+//2
+//let n be the max node, k level of cbt
+//k=2^(k-1)(min)
+//at given cbt at each node, root node & max node are equal ,min heap & max heap need not to be equal
+//mean heapify n max heapify
+//every tree is graph but vica versa not true
+//  o(log n)-> (worst & avg) n o(1)->best case
+//       
